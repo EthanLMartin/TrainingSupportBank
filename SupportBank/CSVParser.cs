0a33413e8692bc16df52c3f9fab47f8e5dbba41a
EthanLMartin <Ethan.Martin@Softwire.com>
2019-09-04 15:37:10 +0100

    Added log messages and error messages

--- SupportBank/CSVParser.cs
+++ SupportBank/CSVParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,32 +9,69 @@ namespace SupportBank
 {
     class CSVParser
     {
+        private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
         public List<Transaction> ParseFile(string directory)
         {
             List<string> lines = System.IO.File.ReadAllLines(directory).ToList();
             List<Transaction> transactions = new List<Transaction>();
 
+            logger.Log(LogLevel.Warn, "First line of CSV should be titles. If the first line is a transaction it will be ignored");
             lines.RemoveAt(0);
 
             foreach (string line in lines)
             {
-                string[] splitLine = line.Split(',');
-                Transaction transaction = CreateTransation(splitLine);
-                transactions.Add(transaction);
+                Transaction transaction = new Transaction();
+                bool success = LineIntoTransaction(line, transaction);
+                if (success)
+                {
+                    transactions.Add(transaction);
+                }
             }
 
             return transactions;
         }
 
-        private Transaction CreateTransation(string[] data)
+        private bool LineIntoTransaction(string line, Transaction transaction)
         {
-            Transaction transaction = new Transaction();
-            transaction.date = DateTime.Parse(data[0]);
+            string[] data = line.Split(',');
+
+            logger.Log(LogLevel.Debug, "At beginning of parsing a transaction line");
+
+            logger.Log(LogLevel.Debug, "Parsing date");
+            try
+            {
+                transaction.date = DateTime.Parse(data[0]);
+            }
+            catch
+            {
+                logger.Log(LogLevel.Error, line + " contains invalid date");
+                Console.WriteLine("ERROR: Invalid date found in line:");
+                Console.WriteLine(line);
+                Console.WriteLine("Please use DD/MM/YYYY");
+                Console.WriteLine();
+                return false;
+            }
+            logger.Log(LogLevel.Debug, "Parsing from, to and narrative");
             transaction.from = data[1];
             transaction.to = data[2];
             transaction.narrative = data[3];
-            transaction.amount = Convert.ToDouble(data[4]);
-            return transaction;
+
+            logger.Log(LogLevel.Debug, "Parsing amount");
+            try
+            {
+                transaction.amount = Convert.ToDouble(data[4]);
+            }
+            catch
+            {
+                logger.Log(LogLevel.Error, line + " contains invalid amount");
+                Console.WriteLine("ERROR: Invalid amount found within line:");
+                Console.WriteLine(line);
+                Console.WriteLine();
+                return false;
+            }
+            logger.Log(LogLevel.Debug, "Reached end of parsing a transaction line");
+            return true;
+
         }
     }
 }
--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -1,16 +1,26 @@
-﻿using System;
+﻿using NLog.Targets;
+using NLog.Config;
+using NLog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace SupportBank
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string directory = @"\Work\Training\SupportBank\Files\Transactions2014.csv";
+            var config = new LoggingConfiguration();
+            var target = new FileTarget { FileName = @"C:\Work\Logs\SupportBank.log", Layout = @"${longdate} ${level} - ${logger}: ${message}" };
+            config.AddTarget("File Logger", target);
+            config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
+            LogManager.Configuration = config;
+
+            //string directory = @"\Work\Training\SupportBank\Files\Transactions2014.csv";
+            string directory = @"\Work\Training\SupportBank\Files\DodgyTransactions2015.csv";
+
             CSVParser parserCSV = new CSVParser();
             List<Transaction> transactions = parserCSV.ParseFile(directory);
             TransactionsRepository accounts = new TransactionsRepository(transactions);
